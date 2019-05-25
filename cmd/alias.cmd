48e1531736b0a1f2674009caa9c30e8dd3190849
Aditya Kamble <adityakamble49@gmail.com>
2019-05-25 15:18:33 +0530

    Windows cmd alias added

---
+++ cmd/alias.cmd
@@ -0,0 +1,5 @@
+:: Virtualenv Activators
+
+DOSKEY pyap="%USERPROFILE%\venv\autire-python\Scripts\activate"
+DOSKEY pyds="%USERPROFILE%\venv\data-science\Scripts\activate"
+DOSKEY pydm="%USERPROFILE%\venv\deploy-manager\Scripts\activate"
---
+++ cmd/install.bat
@@ -0,0 +1,2 @@
+xcopy "alias.cmd" "%USERPROFILE%\alias.cmd"
+install.reg
