6497edc023b7bed9f54d17d3977589ca2b8254c6
Aditya Kamble <adityakamble49@gmail.com>
2021-04-08 11:30:06 -0400

    Vim installer updated

--- install/vim.sh
+++ install/vim.sh
@@ -2,11 +2,3 @@
 
 # install vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# install powerline fonts
-git clone https://github.com/powerline/fonts.git ~/.vim/fonts
-bash ~/.vim/fonts/install.sh
-
-# change locale to en_IN.utf8 to support special characters
-sudo sed -i -e 's/en_IN/en_IN.utf8/1' /etc/default/locale
-echo "reboot system to enable en_IN.utf8"
